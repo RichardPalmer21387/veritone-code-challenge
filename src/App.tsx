ceeb2070e8f675de53dac66d41b942a379197e70
BEHEMOTH\Richard <richard.palmer21387@gmail.com>
2022-01-21 15:31:00 -0600

    Add xo for linting.  Initial setup of header element styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const styles: React.HTMLAttributes<HTMLDivElement>['style'] = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		width: '100%',
+		height: '100%',
+	};
+
+	return (
+		<div className="App" style={styles}>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function Header() {
+	const headerStyles: React.HTMLAttributes<HTMLDivElement>['style'] = {
+		display: 'flex',
+		alignItems: 'center',
+		paddingLeft: '30px',
+		width: '100%',
+		height: '64px',
+		backgroundColor: '#4D81B7',
+		textTransform: 'uppercase',
+		fontFamily: 'Dosis; san-serif',
+		fontSize: '18px',
+		fontStyle: 'normal',
+		fontWeight: 600,
+		letterSpacing: '0.25px',
+		color: 'white',
+	};
+
+	return <header style={headerStyles} className="App-header">
+		Shopping List
+	</header>;
+}
+
+export default Header;

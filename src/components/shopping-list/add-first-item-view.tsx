5dbe249fb7a5a5af291a1186cbd922eb6c701299
BEHEMOTH\Richard <richard.palmer21387@gmail.com>
2022-01-23 18:32:21 -0600

    Add some initial styling and a button for the add first item screen.

--- src/components/shopping-list/add-first-item-view.tsx
+++ src/components/shopping-list/add-first-item-view.tsx
@@ -1,9 +1,31 @@
+import {Button} from '@mui/material';
+import log from 'loglevel';
 import React from 'react';
 
+const styles: React.HTMLAttributes<HTMLDivElement>['style'] = {
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	textAlign: 'center',
+	alignSelf: 'center',
+	border: '1px solid #C6C6C6',
+	borderRadius: '5px',
+	height: '29rem',
+	width: '61.4rem',
+};
+
 export function AddFirstItemView() {
-	return <div className="add-first-item">
-		Your shopping list is empty :(<br/>
-			Add your first item
+	return <div style={styles} className="add-first-item">
+		<div>Your shopping list is empty :(</div>
+		<div>
+			<Button
+				onClick={() => {
+					log.info('Add new item');
+				}}
+			>
+				Add your first item
+			</Button>
+		</div>
 	</div>;
 }
 

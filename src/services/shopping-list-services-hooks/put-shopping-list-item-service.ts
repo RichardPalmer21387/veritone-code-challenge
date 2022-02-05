858476636d44cbfea32245c84b715302f224f61a
BEHEMOTH\Richard <richard.palmer21387@gmail.com>
2022-02-04 19:16:52 -0600

    Make the put service truly offline first update.

--- src/services/shopping-list-services-hooks/put-shopping-list-item-service.ts
+++ src/services/shopping-list-services-hooks/put-shopping-list-item-service.ts
@@ -1,4 +1,4 @@
-import {isNil} from 'lodash';
+import {clone, isNil} from 'lodash';
 import log from 'loglevel';
 import moment from 'moment';
 import {Dispatch, useCallback} from 'react';
@@ -29,60 +29,93 @@ export function usePutShoppingListItemService(dispatch: Dispatch<PatchShoppingIt
 				deleted: updateListItem.deleted,
 			};
 
-			fetch(`/api/${putListItem.id}`, {
-				method: 'PUT',
-				body: JSON.stringify(putListItem),
-				headers,
-			})
-				.then(async response => response.json())
-				.then((response: PutResponseId | ShoppingListDBRow) => {
-					// Open a read/write db transaction, ready for adding the data
-					const transaction = localDB.transaction([VERITONE_SHOPPING_LIST], 'readwrite');
-					// Report on the success of the transaction completing, when everything is done
-					transaction.oncomplete = () => {
-						dispatch({
-							type: ShoppingListActionTypes.PUT_SHOPPING_ITEM,
-							listItem: {
-								...updateListItem,
-								modified,
-							},
-						});
-						resolve();
-					};
+			// Open a read/write db transaction, ready for adding the data
+			const transaction = localDB.transaction([VERITONE_SHOPPING_LIST], 'readwrite');
+			// Report on the success of the transaction completing, when everything is done
+			transaction.oncomplete = () => {
+				dispatch({
+					type: ShoppingListActionTypes.PUT_SHOPPING_ITEM,
+					listItem: {
+						...updateListItem,
+						modified,
+					},
+				});
 
-					transaction.addEventListener('error', () => {
-						reject(transaction.error);
+				log.info('SENDING update request to backend...');
+				fetch(`/api/${putListItem.id}`, {
+					method: 'PUT',
+					body: JSON.stringify(putListItem),
+					headers,
+				})
+					.then(async response => response.json())
+					.then((response: PutResponseId | ShoppingListDBRow) => {
+						// Create modifiable copy of updated list item
+						let responseUpdateListItem = clone(updateListItem);
+						// Open a read/write db transaction, ready for adding the data
+						const transaction = localDB.transaction([VERITONE_SHOPPING_LIST], 'readwrite');
+						// Report on the success of the transaction completing, when everything is done
+						transaction.oncomplete = () => {
+							dispatch({
+								type: ShoppingListActionTypes.PUT_SHOPPING_ITEM,
+								listItem: {
+									...responseUpdateListItem,
+									modified,
+								},
+							});
+							resolve();
+						};
+
+						transaction.addEventListener('error', () => {
+							reject(transaction.error);
+						});
+
+						// Call an object store that's already been added to the database
+						log.info('PUT update data in indexeddb from response', response);
+						const objectStore = transaction.objectStore(VERITONE_SHOPPING_LIST_OBJECT_STORE);
+
+						if (isPutResponseId(response)) {
+							// Make a request to update our newItem object to the object store
+							log.info('PUT BY ID:', putListItem);
+							objectStore.put({
+								...putListItem,
+								id: response.id,
+							});
+						} else {
+							log.info('PUT BY UPDATED RESPONSE ROW:', response);
+							// Or we have a newer version on remote and need to update our local.
+							modified = moment(response.modified);
+
+							// Update our local state to match latest data from server using last-modified conflict resolution.
+							responseUpdateListItem = {
+								...response,
+								id: response.id.toString(),
+								modified: moment(response.modified),
+							};
+
+							objectStore.put({
+								...response,
+								modified: modified.toISOString(),
+								id: response.id.toString(),
+							});
+						}
+					})
+					.catch(error => {
+						reject(error);
 					});
 
-					// Call an object store that's already been added to the database
-					const objectStore = transaction.objectStore(VERITONE_SHOPPING_LIST_OBJECT_STORE);
-					log.info(objectStore.indexNames);
-					log.info(objectStore.keyPath);
-					log.info(objectStore.name);
-					log.info(objectStore.transaction);
-					log.info(objectStore.autoIncrement);
+				resolve();
+			};
 
-					if (isPutResponseId(response)) {
-						// Make a request to update our newItem object to the object store
-						log.info('PUT BY ID:', putListItem);
-						objectStore.put({
-							...putListItem,
-							id: response.id,
-						});
-					} else {
-						log.info('PUT BY UPDATED RESPONSE ROW:', response);
-						// Or we have a newer version on remote and need to update our local.
-						modified = moment(response.modified);
-						objectStore.put({
-							...response,
-							modified: modified.toISOString(),
-							id: response.id.toString(),
-						});
-					}
-				})
-				.catch(error => {
-					reject(error);
-				});
+			transaction.addEventListener('error', () => {
+				reject(transaction.error);
+			});
+
+			// Call an object store that's already been added to the database
+			log.info('PUT update data in indexeddb pre-send', putListItem);
+			const objectStore = transaction.objectStore(VERITONE_SHOPPING_LIST_OBJECT_STORE);
+
+			// Make a request to update our newItem object to the object store
+			objectStore.put(putListItem);
 		}
 	}), [localDB, dispatch]);
 }
